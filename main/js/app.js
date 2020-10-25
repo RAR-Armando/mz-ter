b80c1e45d1773c65922f558215c903e643c6345a
RAR-Armando <70813056+RAR-Armando@users.noreply.github.com>
2020-10-25 22:07:42 +0800

    ver1

---
+++ main/js/app.js
@@ -0,0 +1,5 @@
+const btnThem = document.querySelector(".btn-them")
+const bodyThem = document.querySelector(".body")
+btnThem.addEventListener("click", function () {
+   bodyThem.classList.toggle("black")
+});
